558f49915166b2894fff32bb937f4a2143570b44
Marco Tsui <marcotsui2003@yahoo.com>
2016-07-04 17:21:00 -0400

    add self.from_twitter class method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,22 +21,36 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth, current_user)
     authorization = Authorization.where(:provider => auth.provider, :uid => auth.uid.to_s, :token => auth.credentials.token, :secret => auth.credentials.secret).first_or_initialize
-    if authorization.user.blank? #authorizations created previously must belong to a user(should add dependent :destroy)
-                                 #so authorization.user blank means it is created for the first time
-      user = current_user || (User.where('email = ?', auth["info"]["email"]).first if auth.provider != "twitter") # will break if use twitter without logging in
+    if authorization.user.blank?
+      #authorizations created previously must belong to a user(should add dependent :destroy)
+      #so authorization.user blank means it is created for the first time
+      user = current_user || (User.where('email = ?', auth["info"]["email"]).first) # will break if use twitter without logging in
       if user.blank?
+       # means that this social login not connected to any existing user accounts
+       # and the user is not already logged in
        user = User.new
        user.password = Devise.friendly_token[0,10]
-
-       if auth.provider == "twitter"
-         user.save(:validate => false)
-         # need to redirect to edit profile!!
-       else
-         user.username = auth.info.name
-         user.email = auth.info.email
-         user.save
+       #if auth.provider == "twitter"
+       # user.save(:validate => false)
+       # need to redirect to edit profile!!
+       user.username = auth.info.name
+       user.email = auth.info.email
+       user.save
        end
-     end
+     authorization.username = auth.info.nickname
+     authorization.user_id = user.id
+     authorization.save
+    end
+    authorization.user
+  end
+
+  def self.from_twitter(auth, current_user)
+    authorization = Authorization.where(:provider => auth.provider, :uid => auth.uid.to_s, :token => auth.credentials.token, :secret => auth.credentials.secret).first_or_initialize
+    if authorization.user.blank?
+      user = current_user
+      if user.blank?
+        return User.new
+      end
      authorization.username = auth.info.nickname
      authorization.user_id = user.id
      authorization.save
