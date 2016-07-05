f6c1f2bae1ca58790b2aba24070c0f1d5e9eb468
Marco Tsui <marcotsui2003@yahoo.com>
2016-07-05 11:16:55 -0400

    add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+peter = User.create(email: "peter@123.com", password: "123456", password_confirmation:"123456")
+mary = User.create(email: "mary@123.com", password: "123456", password_confirmation:"123456")
+david = User.create(email: "david@123.com", password: "123456", password_confirmation:"123456")
+
+post1 = peter.posts.create(title: "first post")
+post2 = peter.posts.create(title: "second post")
+
+post1.categories.create(name: "science")
+post1.categories.create(name: "fiction")
+
+post2.categories.create(name: "science")
+post2.categories.create(name: "history")
+
+mary.commented_posts << post1
+
+peter.commented_posts << post2
+
+david.comments.create(post: post2)
