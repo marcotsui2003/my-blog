6bb444177417222ce33ec968ca7e4d4ec8f7924f
Marco Tsui <marcotsui2003@yahoo.com>
2016-07-08 15:47:51 -0400

    add CommentsController with #new and #create actions

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,32 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  def new
+    @post= Post.find_by(id: params[:post_id])
+    #must user build instead of create, otherwise form_for will
+    #be sent to #update action!
+    @comment = @post.comments.build
+  end
+
+  def create
+    post = Post.find(params[:post_id])
+    if post.nil?
+      flash[:alert] = "No such post exists."
+      return redirect_to posts_path
+    else
+      comment = post.comments.build(content: comment_params[:content],
+                                    commenter: current_user)
+      if comment.save
+        flash[:notice] = "Comment successfully created."
+      else
+        flash[:alert] = "Comment fails to be created."
+      end
+      return redirect_to post_path post
+    end
+  end
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
