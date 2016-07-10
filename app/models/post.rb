94bf1138f8ef41c57c5b3d51314bf42a53638b13
Marco Tsui <marcotsui2003@yahoo.com>
2016-07-10 15:33:55 -0400

    add class methods pick_blogger and by_category

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,23 @@ class Post < ActiveRecord::Base
   has_many :comments, inverse_of: :post
   has_many :commenters, through: :comments
   has_many :replies, inverse_of: :post
+
+  def self.pick_blogger(user_id)
+    if user_id.blank?
+      self.all
+    else
+      where("posts.user_id = ?" , user_id )
+    end
+  end
+
+  def self.by_category(category_id)
+    if category_id.blank?
+      self.all
+    else
+      joins(:categories).where("categories.id = ?", category_id)
+    end
+  end
+
+
+
 end
