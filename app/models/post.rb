80fce92e334b88381331e5b0bbaf22f7c207315b
Marco Tsui <marcotsui2003@yahoo.com>
2016-07-11 22:53:48 -0400

    add accepts_nested_attributes_for :catergories to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ActiveRecord::Base
   has_many :commenters, through: :comments
   has_many :replies, inverse_of: :post
 
+  accepts_nested_attributes_for :categories
+
   def self.pick_blogger(user_id)
     if user_id.blank?
       self.all
@@ -22,6 +24,29 @@ class Post < ActiveRecord::Base
     end
   end
 
+  def self.by_date(date)
+    if date.blank?
+      self.all
+    elsif date == "This week"
+      where("posts.created_at >= ?", Time.zone.now - 7.days)
+    elsif date == "Today"
+      where("posts.created_at >= ?", Time.zone.today.beginning_of_day)
+    elsif date =="Older posts"
+      where("posts.created_at < ?", Time.zone.now - 7.days)
+    end
+  end
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_name|
+      category = Category.find_or_create_by(category_name)
+      self.categories << category
+      #below wont work coz @post has not been saved:
+      #self.categories << category unless self.categories.exists?(category)
+      #use category_ids = category_ids.uniq after save..
+    end
+    unique_category_ids = self.category_ids.uniq
+    self.categories.clear
+    self.category_ids = unique_category_ids
+  end
 
 end
