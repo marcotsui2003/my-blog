0554e8ec5a55095353de897ee100749a0dd3b063
Marco Tsui <marcotsui2003@yahoo.com>
2016-07-11 22:50:09 -0400

    modify posts_controller and show view to add selected value in select_tag

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,19 @@
 class PostsController < ApplicationController
+  # need before_action :authenticate_user! own_this_post?
+
+
   def index
-    @posts = Post.pick_blogger(params[:blogger_id]).by_category(params[:category])
+    @blogger_id = params[:blogger_id]
+    @category_id = params[:category_id]
+    @date = params[:date]
+    @posts = Post.pick_blogger(@blogger_id).by_category(@category_id).by_date(@date)
   end
 
   def show
     @post = Post.find(params[:id])
     @comments = @post.comments
   end
+
+
+
 end
