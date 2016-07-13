ae30db9bb349009ca938202d6972e49d9e1a8e5a
Marco Tsui <marcotsui2003@yahoo.com>
2016-07-13 14:56:02 -0400

    add right_to_edit_comment? method as a before-filter

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :right_to_edit_comment?, except: [:index, :show, :new, :create]
+
   def new
     @post= Post.find_by(id: params[:post_id])
     #must user build instead of create, otherwise form_for will
@@ -24,11 +26,21 @@ class CommentsController < ApplicationController
     end
   end
 
-  
+
 
 
   private
   def comment_params
     params.require(:comment).permit(:content)
   end
+
+  def right_to_edit_comment?
+    #can expand here to take care of non-existent posts and comments!
+    comment = Comment.find_by(id: params[:id])
+    unless  comment.commenter == current_user && !comment.nil?
+      return redirect_to post_path(params[:post_id]),
+      alert: "You do not have right to create/modify this comment."
+    end
+  end
+
 end
