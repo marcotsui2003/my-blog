d68334157a88b2b54b0808bc406f72eef67a0cae
Marco Tsui <marcotsui2003@yahoo.com>
2016-07-14 14:07:17 -0400

    add validaton for presence for content in reply.rb

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -3,4 +3,6 @@ class Reply < ActiveRecord::Base
   has_many :replies, as: :repliable
   belongs_to :replier, class_name: "User", inverse_of: :replies
   belongs_to :post, inverse_of: :replies
+
+  validates_presence_of :content
 end
