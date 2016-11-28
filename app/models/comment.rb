38a906890e85f5ae0b045b2c85cabe2807a9f3aa
Marco Tsui <marcotsui2003@yahoo.com>
2016-11-28 16:56:14 -0500

    add dependent :destroy to replies in comments controller

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post, inverse_of: :comments
   belongs_to :commenter, class_name: "User", inverse_of: :comments
-  has_many :replies, as: :repliable
+  has_many :replies, as: :repliable, dependent: :destroy
 
   validates_presence_of :content
 end
