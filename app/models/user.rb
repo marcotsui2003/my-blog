1e58bfe100fd0d5222feb4d331d74601e130b8a2
Marco Tsui <marcotsui2003@yahoo.com>
2016-11-30 12:20:34 -0500

    change post to recipe, category to ingredient in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,11 @@ class User < ActiveRecord::Base
   validates_presence_of :email
 
   has_many :authorizations
-  has_many :posts
-  has_many :categories, through: :posts
+  has_many :recipes
+  has_many :ingredients, through: :recipes
+  #need to use inverse_of if foreign_key or class_name is specified
   has_many :comments, foreign_key: :commenter_id, inverse_of: :commenter
-  has_many :commented_posts, through: :comments, source: :post
+  has_many :commented_recipes, through: :comments, source: :recipe
   #foreign_key is necessary here otherwise replies.user_id is used instead when
   #user.replies is called
   has_many :replies, foreign_key: :replier_id, inverse_of: :replier
@@ -19,7 +20,7 @@ class User < ActiveRecord::Base
   # missing.png should be in assets/images/:style/missing.png
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "25x25#" }, :default_url => ":style/missing.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
-
+  #
   def self.new_with_session(params,session)
     if session["devise.user_attributes"]
       new(session["devise.user_attributes"],without_protection: true) do |user|
@@ -69,4 +70,10 @@ class User < ActiveRecord::Base
     end
     authorization.user
   end
+
+  #def self.ingredients
+  #  recipes.inject([]) do |list, recipe|
+  #    list + recipe.ingredients
+  #  end
+  #end
 end
