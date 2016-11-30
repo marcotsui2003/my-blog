37fc05424c17abb9394552b478fd77853dd49484
Marco Tsui <marcotsui2003@yahoo.com>
2016-11-30 12:19:30 -0500

    change post to recipe, category to ingredient in recipe.rb

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -2,7 +2,7 @@ class Reply < ActiveRecord::Base
   belongs_to :repliable, polymorphic: true
   has_many :replies, as: :repliable
   belongs_to :replier, class_name: "User", inverse_of: :replies
-  belongs_to :post, inverse_of: :replies
+  belongs_to :recipe, inverse_of: :replies
 
   validates_presence_of :content
 end
